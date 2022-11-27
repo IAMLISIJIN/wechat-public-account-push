dcff87ec2e8bd0e82ced2d8d965d98d6783fb95e
IAMLISIJIN <82640198+IAMLISIJIN@users.noreply.github.com>
2022-11-27 15:23:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oH_ZU6M9ppJHHZ10N9XN8PxXAsMI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'CIXFvopiga23bWR6MeCUo3ujesXZ2hN94t_6EBp_Mec',
+      useTemplateId: 'KrconQSa1YZ57z7T6nftWnVAmjiXVb7JrmJpzrpD5B0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-08',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'W1Ff1b0SNqQSjRLLN6pRFU99qqzFA53WMb9Q5UoOYEI',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oH_ZU6GbjoLdhjLcuCu9I3n2n9Jc',
     }
   ],
 
